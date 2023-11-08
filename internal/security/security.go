507c60ee75973591d19aa6d260686da6c2eda112
mauriciodm1998 <mauriciodmpires1@gmail.com>
2023-11-08 10:00:18 -0300

    updated

--- internal/security/security.go
+++ internal/security/security.go
@@ -5,7 +5,3 @@ import "golang.org/x/crypto/bcrypt"
 func Hash(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
-
-func CheckPassword(passwordHash, passwordString string) error {
-	return bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(passwordString))
-}
